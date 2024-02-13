df3451d5a9b29c11269487a1c7e72a4956028635
NAHxiao <xiaohanzero@163.com>
2024-02-13 14:30:03 +0800

    dotfiles_bakup_240213_14:02:03_846924845

--- dotfiles/.scripts/systemd-scripts/dotfiles-bakup.sh
+++ dotfiles/.scripts/systemd-scripts/dotfiles-bakup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+SUFFIX=">/dev/null 2>&1"
+function RUN(){
+    # echo $@
+    eval "$@" $SUFFIX
+}
+[[ "$1" == "-v" ]] && SUFFIX=""
 config_json="$HOME/.scripts/systemd-scripts/dotfiles-bakup-dirs.json"
 BAKUPTO="/mnt/data/backup/dotfiles"
 [[ ! -w $BAKUPTO ]] && BAKUPTO="$HOME/workspace/dotfiles"
@@ -12,27 +18,27 @@ mkdir -p "$REPODOTFILEDIR"
 #init REPO
 cd "$LOCALREPODIR" || { echo "未知错误: cd $LOCALREPODIR Failed" ; exit  2 ; }
 [[ ! -d '.git' ]] &&  {
-echo "Init local repo"
+echo "Init local repo..."
 git init
 git remote add origin $REMOTEREPOLINK
 }
-echo "pull remote repo"
+echo "pull remote repo..."
 git pull origin main --allow-unrelated-histories
 #updata local
-
+echo "update local repo"
 [[ ${#rsyncarray[@]} -eq 0 ]] && { echo "读取配置文件失败或配置为空" ; exit 1 ; }
 rsyncarray[0]=${rsyncarray[0]%/}
 for (( i=1; i<${#rsyncarray[@]}; i++ )) ; do 
 rsyncarray[i]=${rsyncarray[i]%/}
 TODIR="$REPODOTFILEDIR/${rsyncarray[$i]#"${rsyncarray[0]}/"}"
 if [[ -d "${rsyncarray[$i]}" ]] ; then 
-rsync -avz --delete "${rsyncarray[$i]}" "${TODIR%/*}"
+    RUN rsync -avz --delete "${rsyncarray[$i]}" "${TODIR%/*}"
 else
-rsync -avz --delete "${rsyncarray[$i]}" "${TODIR%/*}/"
+    RUN rsync -avz --delete "${rsyncarray[$i]}" "${TODIR%/*}/" 
 fi
 done
 
-
+echo "tar and bakup to $BAKUPTO ..."
 mkdir -p "$BAKUPTO" 2>/dev/null
 if [[ ! -d $BAKUPTO ]] ; then 
 	 echo " 创建备份目录失败" 
@@ -40,13 +46,14 @@ else
 		BAKFILE="dotfiles_bakup_$(date +%y%m%d_%k:%0l:%S_%N).tar.gz"
 		echo "taring to $BAKUPTO/$BAKFILE"
 		cd "$REPODOTFILEDIR" || { echo "未知错误: cd $REPODOTFILEDIR Failed" ; exit  2 ; }
-		tar -czvf $BAKUPTO/"$BAKFILE" .
+        RUN tar -czvf $BAKUPTO/"$BAKFILE" .
 		cd -
 fi
 [[ ! -f $BAKUPTO/"$BAKFILE" ]] && echo -e "\ntar打包失败\n"
 sleep 1 
+echo "add and commit to local repo..."
 git add .
 git commit -m "dotfiles_bakup_$(date +%y%m%d_%k:%0l:%S_%N)"
 git branch -M main
-echo "push to remote main repo "
+echo "push to remote main repo..."
 git push origin main
--- dotfiles/.scripts/systemd-scripts/updata_clash_config.sh
+++ dotfiles/.scripts/systemd-scripts/updata_clash_config.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 FOLDER=$HOME/.config/clash
 # FOLDER=$HOME/.config/clash_win
 FILE=config_$(date +%y%m%d_%k:%0l:%S_%N).yaml
-LINK="https://mojie.app/api/v1/client/subscribe?token=0ce896d17bde60480c9e1a8bb540b29e""&flag=clash"
+WIN_FILE="/mnt/d/Users/Administrator/.config/clash/profiles/1707726507266.yml"
+LINK="https://mojie0201.xn--8stx8olrwkucjq3b.com/api/v1/client/subscribe?token=0ce896d17bde60480c9e1a8bb540b29e""&flag=clash"
 # LINK="https://cn.xfltd.cc/api/v1/client/subscribe?token=b6b5acea2528e94504a39715a9694edd&flag=clash"
+mkdir -p $FOLDER
 wget --no-proxy "$LINK" -O "$FOLDER"/"$FILE"
 # wget "$LINK" -O "$FOLDER"/"$FILE"
 if [[ -f "$FOLDER"/"$FILE"  && -s "$FOLDER"/"$FILE" ]]; then 
@@ -18,6 +21,7 @@ sed -i "/rules/a\    - \'DOMAIN-KEYWORD,anthropic,ChatGPT\'" "$FOLDER"/"$FILE"
 sed -i "/rules/a\    - \'DOMAIN-SUFFIX,chat.openai.com,ChatGPT\'" "$FOLDER"/"$FILE"
 rm "$FOLDER"/config.yaml
 ln -s "$FOLDER"/"$FILE" "$FOLDER"/config.yaml
+cat "$FOLDER"/"$FILE" > "$WIN_FILE"
 echo "更新成功"
 else
 echo "更新失败"

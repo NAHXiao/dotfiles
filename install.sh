5bf210940e66b236cadc07084ba65aa7040c1566
NAHxiao <xiaohanzero@163.com>
2024-03-01 19:22:33 +0800

    Fixed

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash 
-mkdir ~/.config
+mkdir ~/.config 2>/dev/null
 for configdir in $(ls -A1 dotfiles/.config) ;do
     echo delete ~/.config/${configdir} and link ./dotfiles/.config/${configdir} to ~/.config/${configdir}
     rm -rf ~/.config/${configdir}
-    ln -sr $(pwd)/dotfiles/.config/${configdir} ~/.config/${configdir}
+    ln -s $(pwd)/dotfiles/.config/${configdir} ~/.config/${configdir}
 done
 echo delete ~/.scripts and link .scripts to ~/.scripts
 rm -rf ~/.scripts
-ln -sr $(pwd)/dotfiles/.scripts ~/.scripts
+ln -s $(pwd)/dotfiles/.scripts ~/.scripts
 echo delete ~/scripts and link ~/.scripts to ~/scripts
 rm -rf ~/scripts
-ln -sr ~/.scripts ~/scripts
+ln -s ~/.scripts ~/scripts
 
 for configfile in $(/bin/ls -A1 dotfiles|grep -v '^\.config$');do
         echo delete ~/${configfile} and link ./dotfiles/${configfile} to ~/${configfile}
         rm -rf ~/${configfile}
-        ln -sr $(pwd)/dotfiles/${configfile} ~/${configfile}
+        ln -s $(pwd)/dotfiles/${configfile} ~/${configfile}
 done

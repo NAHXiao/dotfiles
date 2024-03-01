be67da2698e21d6940256e9ac5e57591f9ec2ef3
NAHxiao <xiaohanzero@163.com>
2024-03-01 19:05:45 +0800

    Fixed install script

--- install.sh
+++ install.sh
@@ -3,18 +3,17 @@
 for configdir in $(ls dotfiles/.config) ;do
     echo delete ~/.config/${configdir} and link ./dotfiles/.config/${configdir} to ~/.config/${configdir}
     rm -rf ~/.config/${configdir}
-    ln -s $(pwd)/dotfiles/.config/${configdir} ~/.config/${configdir}
+    ln -sr $(pwd)/dotfiles/.config/${configdir} ~/.config/${configdir}
 done
 echo delete ~/.scripts and link .scripts to ~/.scripts
 rm -rf ~/.scripts
-ln -s $(pwd)/dotfiles/.scripts ~/.scripts
+ln -sr $(pwd)/dotfiles/.scripts ~/.scripts
 echo delete ~/scripts and link ~/.scripts to ~/scripts
-ln -s ~/.scripts ~/scripts
+rm -rf ~/scripts
+ln -sr ~/.scripts ~/scripts
 
-for configfile in $(/bin/ls -Fa1);do
-    if [ -f $configfile ];then
+for configfile in $(/bin/ls -Fa1 dotfiles);do
         echo delete ~/${configfile} and link ./dotfiles/${configfile} to ~/${configfile}
         rm -rf ~/${configfile}
-        ln -s $(pwd)/dotfiles/${configfile} ~/${configfile}
-    fi
+        ln -sr $(pwd)/dotfiles/${configfile} ~/${configfile}
 done

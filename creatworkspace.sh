81ea222a42a2fdd69f04cbe9ece81abf0276b838
NAHxiao <xiaohanzero@163.com>
2024-03-21 19:48:03 +0800

    workspace creat.sh

--- creatworkspace.sh
+++ creatworkspace.sh
@@ -3,4 +3,5 @@ mkdir -p $WORKSPACE $WORKSPACE/learn $WORKSPACE/leetcode
 for i in 'c++' 'java' 'python' 'rust' ;do
 mkdir -p $WORKSPACE/$i $WORKSPACE/$i/learn $WORKSPACE/$i/leetcode
 ln -s $WORKSPACE/$i/leetcode $WORKSPACE/leetcode/$i
+ln -s $WORKSPACE/$i/learn $WORKSPACE/learn/$i
 done

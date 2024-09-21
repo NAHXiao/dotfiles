bb5dcf0ac34254c50ef2c44392949678febe994a
NAHxiao <xiaohanzero@163.com>
2024-09-21 23:43:02 +0800

    clangd:config.yaml ; nvim clangd args update

--- dot____custom_dirs_for_link___/nvim/lua/plugins/lsp/mason_lspconfig.lua
+++ dot____custom_dirs_for_link___/nvim/lua/plugins/lsp/mason_lspconfig.lua
@@ -31,9 +31,19 @@ return {
         require("lspconfig").clangd.setup {
             cmd = {
                 "clangd",
-                "--offset-encoding=utf-16",
-                --解决warning: multiple different client offset_encodings detected for buffer, this is not supported yet
-                "--fallback-style=webkit"
+                "--offset-encoding=utf-16", -- 解决warning: multiple different client offset_encodings detected for buffer, this is not supported yet
+                "--fallback-style=webkit",
+                "--enable-config",
+                "--print-options",
+                "--background-index",
+                "--clang-tidy",
+                "--pch-storage=memory",
+                -- "--header-insertion=never",
+                -- "--header-insertion-decorators",
+                "--all-scopes-completion",
+                "--completion-style=detailed",
+                "--log=verbose",
+                "-j=4",
             },
         }
         require('lspconfig').sqlls.setup {

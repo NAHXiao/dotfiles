e2f8699edf5e926d79d87b9f770d77161aec7649
NAHxiao <xiaohanzero@163.com>
2024-10-23 21:32:39 +0800

    del some conf

--- dot_scripts/executable_close_rofi_while_not_focus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-sleep 0.5
-[[ -z $1 ]] && set -- "$(pgrep -nx rofi)"
-[[ -z $1 ]] &&	echo "no rofi process found" && exit 1
-echo $1
-while kill -0 "$1" 2>/dev/null; do 
-[[ $(hyprctl activewindow -j | jq  -r .class) != "Rofi" ]] && killall rofi
-sleep 0.1
-done

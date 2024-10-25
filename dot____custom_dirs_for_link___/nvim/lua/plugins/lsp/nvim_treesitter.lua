5a912bcc9a4b678fe58173b8dc87caedb7d56d08
NAHxiao <xiaohanzero@163.com>
2024-10-25 15:56:55 +0800

    disable treesitter.highlight for md

--- dot____custom_dirs_for_link___/nvim/lua/plugins/lsp/nvim_treesitter.lua
+++ dot____custom_dirs_for_link___/nvim/lua/plugins/lsp/nvim_treesitter.lua
@@ -40,22 +40,22 @@ return {
             -- TODO if true  some .d will open failed
             auto_install = true,
             highlight = {
-                -- 打开会导致python缩进异常
                 enable = true,
-                -- enable = false,
+                -- 打开会导致python缩进异常/markdown <span>标签键入title时闪退
+                disable = { "markdown", "python"},
             },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<S-Tab>",  -- normal mode
-                    node_incremental = "<Tab>",  -- visual mode
-                    node_decremental = "<S-Tab", -- visual mode
-                },
-            },
-            ident = { enable = true },
-            rainbow = {
-                enable = true,
-            }
+            -- incremental_selection = {
+            --     enable = true,
+            --     keymaps = {
+            --         init_selection = "<S-Tab>",  -- normal mode
+            --         node_incremental = "<Tab>",  -- visual mode
+            --         node_decremental = "<S-Tab", -- visual mode
+            --     },
+            -- },
+            -- -- ident = { enable = true },
+            -- rainbow = {
+            --     enable = true,
+            -- }
         }
     end,
 }

f2efda6286ea7f77659b53d01f0c56554c75a26f
NAHxiao <xiaohanzero@163.com>
2024-11-01 14:31:58 +0800

    disable illuminate when fz>500Kb

--- dot____custom_dirs_for_link___/nvim/lua/plugins/ui/vim_illuminate.lua
+++ dot____custom_dirs_for_link___/nvim/lua/plugins/ui/vim_illuminate.lua
@@ -12,8 +12,15 @@ return {
                 -- 'treesitter',
                 'regex',
             },
-            max_file_lines = 3000
-            -- should_enable = function(bufnr) return true end
+            delay = 100,
+            under_cursor = true,
+            max_file_lines = 3000,
+            should_enable = function(bufnr)
+                local last_line = vim.api.nvim_buf_line_count(bufnr)
+                -- 获取最后一行结束的偏移量,即为整个buffer的大小(字节)
+                local size = vim.api.nvim_buf_get_offset(bufnr, last_line)
+                return (size / 1024) <= 500 --NOTE: <=500k则True
+            end,
         })
     end,
 }

97f1bf618d3cabfe2bf7269ffa4ac0df31f20d51
NAHxiao <xiaohanzero@163.com>
2025-06-09 20:03:56 +0800

    latest commit

--- .config/nvim/lua/plugins/ui/nvim_transparent.lua
+++ .config/nvim/lua/plugins/ui/nvim_transparent.lua
@@ -4,14 +4,31 @@ return {
     lazy = false,
     cmd = { "TransparentToggle" },
     keys = {
-        { "<leader>\\", ":TransparentToggle<CR>", mode = "n" },
+        { "<leader>\\", ":TransparentToggle<CR>", mode = "n", silent = true },
     },
     config = function()
         require("transparent").setup({
             exclude_groups = {
                 "CursorLine",
-                "CursorLine",
             },
         })
+        local function set_nr_color()
+            if vim.g.transparent_enabled then
+                vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff" })
+                vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffff" })
+            end
+        end
+        vim.defer_fn(function()
+            set_nr_color()
+        end, 1000)
+        vim.cmd([[
+            delcommand TransparentEnable
+            delcommand TransparentDisable
+            delcommand TransparentToggle
+        ]])
+        vim.api.nvim_create_user_command("TransparentToggle", function()
+            require("transparent").toggle()
+            set_nr_color()
+        end, {desc="Toggle transparent"})
     end,
 }

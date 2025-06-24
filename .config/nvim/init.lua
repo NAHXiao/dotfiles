2011c82840580281bdc118f111e40495c5318450
NAHxiao <xiaohanzero@163.com>
2025-06-24 19:09:13 +0800

    latest commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,9 +4,6 @@ vim.g.mapleader = " "
 vim.g.localleader = " "
 vim.o.timeoutlen = 800
 vim.cmd([[
-set mousemodel=popup
-aunmenu PopUp.How-to\ disable\ mouse
-aunmenu PopUp.-2-
 ]])
 vim.api.nvim_set_hl(0, "TRANSPARENT", { bg = "NONE" })
 -- IMPORTS

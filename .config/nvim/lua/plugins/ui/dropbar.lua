046a8a67a7bb89de750386c111f30dc4c418cfc7
NAHxiao <xiaohanzero@163.com>
2025-07-06 06:29:59 +0800

    latest commit

--- .config/nvim/lua/plugins/ui/dropbar.lua
+++ .config/nvim/lua/plugins/ui/dropbar.lua
@@ -1,41 +1,42 @@
 return {
-    'Bekaboo/dropbar.nvim',
+    "Bekaboo/dropbar.nvim",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    cond = vim.fn.has("nvim-0.11") == 1,
     opts = {
         bar = {
             enable = function(buf, win, _)
-	        if
-		    not vim.api.nvim_buf_is_valid(buf)
-		    or not vim.api.nvim_win_is_valid(win)
-		    or vim.fn.win_gettype(win) ~= ''
-		    or vim.wo[win].winbar ~= ''
-		    or vim.bo[buf].ft == 'help'
-		    or vim.bo[buf].ft == 'neo-tree-preview'
-		    or vim.w[win].neo_tree_preview == 1
-	        then
-		    return false
-	        end
+                if
+                    not vim.api.nvim_buf_is_valid(buf)
+                    or not vim.api.nvim_win_is_valid(win)
+                    or vim.fn.win_gettype(win) ~= ""
+                    or vim.wo[win].winbar ~= ""
+                    or vim.bo[buf].ft == "help"
+                    or vim.bo[buf].ft == "neo-tree-preview"
+                    or vim.w[win].neo_tree_preview == 1
+                then
+                    return false
+                end
 
-	        local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
-	        if stat and stat.size > 1024 * 1024 then
-		    return false
-	        end
+                local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+                if stat and stat.size > 1024 * 1024 then
+                    return false
+                end
 
-	        return vim.bo[buf].ft == 'markdown'
-		    or pcall(vim.treesitter.get_parser, buf)
-		    or not vim.tbl_isempty(vim.lsp.get_clients({
-		        bufnr = buf,
-		        method = 'textDocument/documentSymbol',
-		    }))
-	    end,
+                return vim.bo[buf].ft == "markdown"
+                    or pcall(vim.treesitter.get_parser, buf)
+                    or not vim.tbl_isempty(vim.lsp.get_clients({
+                        bufnr = buf,
+                        method = "textDocument/documentSymbol",
+                    }))
+            end,
         },
         sources = {
             path = {
-                preview = false
-            }
-        }
+                preview = false,
+            },
+        },
     },
-    config = function (_, opts)
+    config = function(_, opts)
         require("dropbar").setup(opts)
 
         local set_dropbar_colors = function()
@@ -48,11 +49,11 @@ return {
         set_dropbar_colors()
         vim.api.nvim_create_autocmd("ColorScheme", {
             pattern = "*",
-            callback = set_dropbar_colors
+            callback = set_dropbar_colors,
         })
         vim.api.nvim_create_autocmd("OptionSet", {
             pattern = "background",
-            callback = set_dropbar_colors
+            callback = set_dropbar_colors,
         })
-    end
+    end,
 }

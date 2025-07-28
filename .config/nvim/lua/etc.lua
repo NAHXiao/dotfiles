51ca485ef26121849a62f7c1ed31566bc2ca00ea
NAHxiao <xiaohanzero@163.com>
2025-07-28 21:01:13 +0800

    latest commit

--- .config/nvim/lua/etc.lua
+++ .config/nvim/lua/etc.lua
@@ -258,7 +258,8 @@ vim.api.nvim_create_autocmd("WinEnter", {
             local ft = vim.fn.getbufvar(bufnr, "&ft")
             local bt = vim.fn.getbufvar(bufnr, "&bt")
             -- require("utils").log("ft:", ft, "bt:", bt, "name:", vim.api.nvim_buf_get_name(bufnr))
-            if not (vim.list_contains(fts, ft) or bt == "nofile") then
+            -- if not (vim.list_contains(fts, ft) or bt == "nofile") then
+            if not vim.list_contains(fts, ft) then
                 return
             end
         end

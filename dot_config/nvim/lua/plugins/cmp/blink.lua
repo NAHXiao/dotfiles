ff3dbf827ae7b32a07a3944201607c64eb27492a
NAHxiao <xiaohanzero@163.com>
2025-08-14 20:27:04 +0800

    blinkcmpMenuSelection

--- dot_config/nvim/lua/plugins/cmp/blink.lua
+++ dot_config/nvim/lua/plugins/cmp/blink.lua
@@ -155,7 +155,7 @@ return {
         snippets = {},
     },
     init = function()
-        require("tools.hl").add_transparent_groupprefix({ "BlinkCmp" })
+        require("tools.hl").add_transparent_groups({ "BlinkCmpMenuBorder" })
     end,
     config = function(_, opts)
         require("blink.cmp").setup(opts)
@@ -172,6 +172,15 @@ return {
                 vim.b.copilot_suggestion_hidden = false
                 vim.cmd("doautocmd CursorMovedI")
             end,
+        })
+        require("tools.hl").regist_transparent({
+            BlinkCmpMenuSelection = {
+                bg = {
+                    transform = "lighten",
+                    from = "BlinkCmpMenuSelection.bg",
+                    amount = 0.3
+                }
+            }
         })
     end,
 }
